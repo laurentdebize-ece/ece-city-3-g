d62efc71b7e679628e6f10a3a39cfc8542872e7a
Lucas A <game4allyt@gmail.com>
2022-10-31 19:44:09 +0100

    Ajout structure de base pour la gestion d'écrans de jeu.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include "raylib.h"
+#include "jeu.h"
+
+const ScreenInfo_t EMPTY_SCREEN = {
+        .draw_callback = NULL,
+        .update_callback = NULL,
+        .on_enter_callback = NULL,
+        .on_exit_callback = NULL,
+        .screen_name = "VIDE"
+};
 
 int main() {
-    InitWindow(800, 600, "slaut");
-
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        EndDrawing();
-    }
+    Jeu_t jeu = {
+            .should_exit = false,
+            .screen = NULL
+    };
+    jeu_run(&jeu, &EMPTY_SCREEN);
 }
