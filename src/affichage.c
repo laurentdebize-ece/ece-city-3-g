327ee8ebf50c85fd603c047ffdef26ffbafe5d9b
Lucas A <game4allyt@gmail.com>
2022-11-07 22:45:30 +0100

    Placement de tous les bâtiments WIP.

--- src/affichage.c
+++ src/affichage.c
@@ -1,5 +1,6 @@
 
 #include "affichage.h"
+#include "sim/chateau.h"
 
 void affichage_draw_terrain_background(SpriteSheet_t* sheet, SimWorld_t* world) {
     for (int i = 0; i < 45; ++i) {
@@ -25,5 +26,19 @@ void affichage_draw_entities(SpriteSheet_t* sheet, SimWorld_t* world) {
         Habitation_t * hab = (Habitation_t*) maison->data;
         sprite_sheet_draw_sprite(sheet, MAISON_3X3, WHITE, hab->position.x, hab->position.y);
         maison = maison->next;
+    }
+
+    struct Maillon_t* centrales = world->centrales->premier;
+    while (centrales) {
+        CentraleElectrique_t* centrale = (CentraleElectrique_t*)centrales->data;
+        sprite_sheet_draw_sprite(sheet, ENERGY_6X4, WHITE, centrale->position.x, centrale->position.y);
+        centrales = centrales->next;
+    }
+
+    struct Maillon_t* chateaux = world->chateaux->premier;
+    while (chateaux) {
+        ChateauEau_t* chateau = (ChateauEau_t*) chateaux->data;
+        sprite_sheet_draw_sprite(sheet, EAU_4X6, WHITE, chateau->position.x, chateau->position.y);
+        chateaux = chateaux->next;
     }
 }
