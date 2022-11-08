9c4f57d7f161a9eeb64c5a279b89ac0b1124c7cb
Lucas A <game4allyt@gmail.com>
2022-11-08 10:31:24 +0100

    Renommer + trier les habitations par distance à l'origine lors du dessin.

--- include/utils/liste.h
+++ include/utils/liste.h
@@ -34,6 +34,9 @@ bool liste_estVide(Liste_t *liste);
 /// Ajoute un élément à la fin de la liste.
 void liste_ajouter_fin(Liste_t *liste, void *data);
 
+/// Ajoute un élément à la liste en respectant l'ordre d'insertion.
+void liste_ajout_tri(Liste_t *liste, void *data, int (*comparateur)(void *, void *));
+
 /// Supprime un élément en fin de liste et le retourne.
 void* liste_supprimer_fin(Liste_t *liste);
 
--- src/sim/habitation.c
+++ src/sim/habitation.c
@@ -93,3 +93,22 @@ int habitation_get_nb_habitants(Habitation_t* habitation) {
 int habitation_cmp(Habitation_t* habitation1, Habitation_t* habitation2) {
     return habitation1->niveau - habitation2->niveau;
 }
+
+int habitation_tri_par_distance(Habitation_t* a, Habitation_t* b)
+{
+    Habitation_t *hab_a = (Habitation_t *) a;
+    Habitation_t *hab_b = (Habitation_t *) b;
+    if (hab_a->position.x < hab_b->position.x) {
+        return -1;
+    } else if (hab_a->position.x > hab_b->position.x) {
+        return 1;
+    } else {
+        if (hab_a->position.y < hab_b->position.y) {
+            return -1;
+        } else if (hab_a->position.y > hab_b->position.y) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
