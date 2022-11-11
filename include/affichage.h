78ad82c15ce237de8ff0741e5c44d8b14f225eb8
Lucas A <game4allyt@gmail.com>
2022-11-11 16:21:18 +0100

    Les bâtiments évoluent maintenant au bout de 15 secondes.

--- include/affichage.h
+++ include/affichage.h
@@ -12,6 +12,9 @@ void affichage_draw_terrain_background(SpriteSheet_t* sheet, SimWorld_t* world);
 /// Dessine les tuiles de la carte.
 void affichage_draw_entities(SpriteSheet_t* sheet, SimWorld_t* world);
 
+/// Dessine une habitation sur la carte.
+void affichage_draw_habitation(SpriteSheet_t* sheet, Habitation_t* habitation);
+
 /// Dessine l'apercu de construction d'un bâtiment sur la carte.
 void affichage_draw_build_preview(SpriteSheet_t* sheet, SimWorld_t* world, Vector2I pos, CaseKind_t type);
 
